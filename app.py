70c4ee4af8f640ef5d899dd81eae45f1e6448983
Creep2701 <roblonczek@gmail.com>
2024-01-18 17:04:43 +0100

    fix app

--- app.py
+++ app.py
@@ -175,23 +175,17 @@ def main():
             processed_image = Image.fromarray(processed_image)
 
         # After processing the image
-# After processing the image
         if processed_image is not None:
             try:
                 # Convert the processed image to RGB mode
                 processed_image_rgb = processed_image.convert("RGB")
 
-                # Save the RGB image as JPEG
+                # Save the RGB image as PNG
                 processed_image_rgb.save(image_path, format="PNG")
 
                 st.image(processed_image_rgb, caption='Processed Image', use_column_width=True)
             except Exception as e:
                 st.error(f"An error occurred when displaying the image: {e}")
 
-
-
-
-    
 if __name__ == "__main__":
     main()
-
