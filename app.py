b545546ab8bd672bcfb066d672db62df55e091c2
René Oblonczek <56590572+Rene27-01@users.noreply.github.com>
2024-01-25 19:15:56 +0100

    final fix

--- app.py
+++ app.py
@@ -37,8 +37,11 @@ def apply_elliptical_mask(image_path):
 
     return final_image
 
+
 def plot_one_box(box, img, color, label=None, line_thickness=None):
-    tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
+    tl = (
+        line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1
+    )  # line/font thickness
     c1, c2 = (int(box[0]), int(box[1])), (int(box[2]), int(box[3]))
     cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
     if label:
@@ -46,16 +49,31 @@ def plot_one_box(box, img, color, label=None, line_thickness=None):
         t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
         c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
         cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
-        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+        cv2.putText(
+            img,
+            label,
+            (c1[0], c1[1] - 2),
+            0,
+            tl / 3,
+            [225, 255, 255],
+            thickness=tf,
+            lineType=cv2.LINE_AA,
+        )
+
 
 def perform_segmentation(segmentation_model_path, image_path):
     segmentation_model = YOLO(segmentation_model_path)
     segmentation_results = segmentation_model.predict(image_path, retina_masks=True)
 
     # Assuming one primary result
-    im_array = segmentation_results[0].plot(line_width=1, conf=0.5, boxes=False)  # BGR numpy array of predictions
-    segmentation_image = Image.fromarray(im_array[..., ::-1])  # Convert to RGB PIL image
+    im_array = segmentation_results[0].plot(
+        line_width=1, conf=0.5, boxes=False
+    )  # BGR numpy array of predictions
+    segmentation_image = Image.fromarray(
+        im_array[..., ::-1]
+    )  # Convert to RGB PIL image
     return segmentation_image
+
 
 def preprocess_and_predict(image_path, detection_model_path, segmentation_model_path):
     try:
@@ -71,12 +89,14 @@ def preprocess_and_predict(image_path, detection_model_path, segmentation_model_
 
         # Apply the elliptical mask to preprocess the image
         preprocessed_image = apply_elliptical_mask(image_path)
-        preprocessed_image_path = 'temp_preprocessed.png'
+        preprocessed_image_path = "temp_preprocessed.png"
         cv2.imwrite(preprocessed_image_path, preprocessed_image)
 
         # Load the YOLO model for object detection
         detection_model = YOLO(detection_model_path)
-        detection_results = detection_model.predict(source=preprocessed_image_path, conf=0.55)
+        detection_results = detection_model.predict(
+            source=preprocessed_image_path, conf=0.55
+        )
 
         # Debugging: Print the shape and data type of the image
         print("Target image shape:", target_image_np.shape)
@@ -86,23 +106,34 @@ def preprocess_and_predict(image_path, detection_model_path, segmentation_model_
         for r in detection_results:
             for detection in r.boxes.data:
                 x1, y1, x2, y2, conf, cls_id = detection
-                label = f'{r.names[int(cls_id)]} {conf:.2f}'
-                plot_one_box([x1, y1, x2, y2], target_image_np, label=label, color=(255, 0, 0), line_thickness=2)
+                label = f"{r.names[int(cls_id)]} {conf:.2f}"
+                plot_one_box(
+                    [x1, y1, x2, y2],
+                    target_image_np,
+                    label=label,
+                    color=(255, 0, 0),
+                    line_thickness=2,
+                )
 
         # Convert back to RGB format for display
         final_image = cv2.cvtColor(target_image_np, cv2.COLOR_BGR2RGB)
         final_image = Image.fromarray(final_image)
-        
+
         # Display intermediate results for debugging
-        st.image(target_image, caption='Segmented Image', use_column_width=True)
-        st.image(Image.fromarray(preprocessed_image), caption='Preprocessed Image', use_column_width=True)
-        
+        st.image(target_image, caption="Segmented Image", use_column_width=True)
+        st.image(
+            Image.fromarray(preprocessed_image),
+            caption="Preprocessed Image",
+            use_column_width=True,
+        )
+
         # Display the final processed image
-        st.image(final_image, caption='Final Processed Image', use_column_width=True)
-        
+        st.image(final_image, caption="Final Processed Image", use_column_width=True)
+
     except Exception as e:
         # Handle any exceptions that may occur
         print(f"An error occurred: {e}")
+
 
 import streamlit as st
 import requests
@@ -121,7 +152,9 @@ def load_image_from_url(url):
             return image  # Return the image object for further processing
         else:
             # Display an error message if the request was not successful
-            st.error("Failed to fetch the image. Status code: " + str(response.status_code))
+            st.error(
+                "Failed to fetch the image. Status code: " + str(response.status_code)
+            )
             return None
 
     except Exception as e:
@@ -132,9 +165,9 @@ def load_image_from_url(url):
 
 def save_uploaded_file(uploaded_file):
     try:
-        with open(os.path.join("tempDir",uploaded_file.name),"wb") as f:
+        with open(os.path.join("tempDir", uploaded_file.name), "wb") as f:
             f.write(uploaded_file.getbuffer())
-        return os.path.join("tempDir",uploaded_file.name)
+        return os.path.join("tempDir", uploaded_file.name)
     except Exception as e:
         return None
 
@@ -143,13 +176,15 @@ def assemble_model_file(parts_folder, original_filename):
     temp_dir = tempfile.mkdtemp()
     assembled_file_path = os.path.join(temp_dir, original_filename)
 
-    with open(assembled_file_path, 'wb') as assembled_file:
+    with open(assembled_file_path, "wb") as assembled_file:
         part_number = 1
         while True:
-            part_filename = f"{os.path.join(parts_folder, original_filename)}.part{part_number}"
+            part_filename = (
+                f"{os.path.join(parts_folder, original_filename)}.part{part_number}"
+            )
             if not os.path.exists(part_filename):
                 break
-            with open(part_filename, 'rb') as part_file:
+            with open(part_filename, "rb") as part_file:
                 assembled_file.write(part_file.read())
             part_number += 1
 
@@ -162,9 +197,12 @@ def assemble_model_file(parts_folder, original_filename):
 
 def main():
     st.title("YOLO Image Processing App")
-    st.markdown("This app uses a YOLOv8 ensemble model to perform object detection and segmentation on dental xray images.")
+    st.markdown(
+        "This app uses a YOLOv8 ensemble model to perform object detection and segmentation on dental xray images."
+    )
 
-    st.markdown("""
+    st.markdown(
+        """
     ## How to Use the Model
     1. **Choose an Image**: Select one of the sample images below or use your own by uploading it or providing a URL.
     2. **Upload or Enter URL**: Use the uploader to upload your image or enter the URL of the image.
@@ -173,61 +211,69 @@ def main():
     **Note 1**: The model may take a few seconds to process the image. Please be patient.
     
     **Note 2**: These images are taken from the web and are not training data.
-    """)
+    """
+    )
 
     st.markdown("Here are some example images to try out:")
-    cols = st.columns(5)
+
     urls = [
         "https://smb.ibsrv.net/imageresizer/image/article_manager/1200x1200/14623/843041/heroimage0.585829001666883203.jpg",
         "https://www.drtuanpham.com/wp-content/uploads/2020/09/pano.jpg",
         "https://millerhilldental.com/wp-content/uploads/2021/05/X-ray.png",
         "https://dentalhealthtoday.com/wp-content/uploads/2723043.jpg",
-        "https://smilesbybis.com/wp-content/uploads/2021/09/x-rays1.png"
+        "https://smilesbybis.com/wp-content/uploads/2021/09/x-rays1.png",
     ]
 
-    for i, url in enumerate(urls):
-        with cols[i % 5]:
-            st.image(url, width=150)  # Set the width as needed
+    cols = st.columns(len(urls))  # Create a column for each image
+    for col, url in zip(cols, urls):
+        with col:
+            st.image(url, width=150)
 
     # Assign each URL to a column
     for i, col in enumerate(cols):
         with col:
             st.markdown(f"[Image {i + 1}]({urls[i]})")
-    parts_folder = 'split_files/'
-    segmentation_model_path = assemble_model_file(parts_folder, 'best-segmentation-m.pt')
-    detection_model_path = assemble_model_file(parts_folder, 'best-detection-xl.pt')
+    parts_folder = "split_files/"
+    segmentation_model_path = assemble_model_file(
+        parts_folder, "best-segmentation-m.pt"
+    )
+    detection_model_path = assemble_model_file(parts_folder, "best-detection-xl.pt")
 
     if not (segmentation_model_path and detection_model_path):
         st.error("Failed to assemble model files. Please check the file paths.")
         return
-    
-    option = st.selectbox("How would you like to provide the image?", ['Upload', 'URL'])
+
+    option = st.selectbox("How would you like to provide the image?", ["Upload", "URL"])
     image_path = None
 
-    if option == 'Upload':
-        uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+    if option == "Upload":
+        uploaded_file = st.file_uploader(
+            "Choose an image...", type=["jpg", "png", "jpeg"]
+        )
         if uploaded_file is not None:
             # Open and convert the uploaded image to JPEG format
             pil_image = Image.open(uploaded_file)
             image_path = "temp_image.jpg"
             pil_image.save(image_path, "JPEG")
-            st.image(pil_image, caption='Loaded Image', use_column_width=True)
+            st.image(pil_image, caption="Loaded Image", use_column_width=True)
 
-    elif option == 'URL':
+    elif option == "URL":
         url = st.text_input("Enter the URL of the image")
         if url:
             image = load_image_from_url(url)
             if image:
                 image_path = "temp_image.png"
                 image.save(image_path)
-                st.image(image, caption='Loaded Image', use_column_width=True)
+                st.image(image, caption="Loaded Image", use_column_width=True)
     if image_path is not None and st.button("Run Model"):
         # Image Processing and Visualization
         processed_image = None
         if image_path:
-           # segmentation_model_path = 'best-segmentation-m.pt'
-            #detection_model_path = 'best-detection-xl.pt'
-            processed_image = preprocess_and_predict(image_path, detection_model_path, segmentation_model_path)
+            # segmentation_model_path = 'best-segmentation-m.pt'
+            # detection_model_path = 'best-detection-xl.pt'
+            processed_image = preprocess_and_predict(
+                image_path, detection_model_path, segmentation_model_path
+            )
 
             if isinstance(processed_image, np.ndarray):
                 processed_image = Image.fromarray(processed_image)
@@ -235,10 +281,14 @@ def main():
             if processed_image is not None:
                 try:
                     processed_image.save("debug_processed_image.png")
-                    st.image(processed_image, caption='Processed Image', use_column_width=True)
+                    st.image(
+                        processed_image,
+                        caption="Processed Image",
+                        use_column_width=True,
+                    )
                 except Exception as e:
                     st.error(f"An error occurred when displaying the image: {e}")
 
+
 if __name__ == "__main__":
     main()
-
